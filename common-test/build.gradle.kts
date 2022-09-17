df142b9094e05b68cc5f89b9c9595ebba6ca2b58
Alaksiej Ščarbaty <hahaha.funny.yellow.dog@gmail.com>
2022-09-17 16:03:22 +0200

    [BE] Dependencies upgrade

--- common-test/build.gradle.kts
+++ common-test/build.gradle.kts
@@ -1,3 +1,4 @@
+val jsoupVersion: String by project
 val mockkVersion: String by project
 val restAssuredKotlinExtensionsVersion: String by project
 
@@ -9,4 +10,5 @@ dependencies {
     api("io.rest-assured:rest-assured")
     api("io.rest-assured:kotlin-extensions:$restAssuredKotlinExtensionsVersion")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+    api("org.jsoup:jsoup:$jsoupVersion")
 }
